c51cdcd16598aa92c238955e247ea70195489718
liz <=>
2017-02-09 13:36:41 -0600

    El index ya esta terminado, el Paso1 esta por comenzar

---
+++ Paso1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PASO 1</title>
+</head>
+<body>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,13 +1,64 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>receta de cocina</title>
+<link rel="stylesheet" type="text/css" href="materialize/css/materialize.min.css">
+	<title>RECETA DE PASTEL DE CHOCOLATE</title>
 </head>
 <body>
 
-<h1>Paso 1:</h1>
+<div class="row ">
+<div class="col s1"></div>
+<div class="col s10">
+<h4 class="card-panel teal lighten-2 center-align">INGREDIENTES</h4>
 
+<table class="bordered">
+	<tr>
+		<th> Cantidad</th>
+		<th> Producto</th>
+	</tr>
+	<tr>
+		<td>5</td>
+		<td>huevos</td>
+	</tr>
+	<tr>
+		<td>1 taza</td>
+		<td> leche condenzada</td>
+	</tr>
+	<tr>
+		<td>1 taza</td>
+		<td>leche evaporada</td>
+	</tr>
+	<tr>
+		<td>2 cucharadas</td>
+		<td>mantequilla</td>
+	</tr>
+	<tr>
+		<td>1 cucharada</td>
+		<td>vainilla</td>
+	</tr>
+</table>
+<div class="col s12 m7">
+    <h2 class="header">Vista preliminar</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="http://www.hersheys.com.mx/postres/assets/images/mexico_postres/Recipes/36.jpg">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>Rendimiento: 14 porciones
+			Tiempo de Preparación: 40 minutos</p>
+        </div>
+        <div class="card-action">
+          <a href="paso1.php">Preparación</a>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+ 
+</div>
 
-
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="materialize/js/materialize.min.js"></script>
 </body>
 </html> 
