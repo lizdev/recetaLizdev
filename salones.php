ee0b6849a2bc57920936962adfadb4d78d466e7a
liz <=>
2017-02-23 16:04:39 -0600

    Grid de los salones

---
+++ salones.php
@@ -0,0 +1,80 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<link rel="stylesheet" type="text/css" href="materialize/css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/icon?family=Material+Icons">
+
+ 	<title>SALONES DE CLASES</title>
+ </head>
+ <body>
+ 
+ <div class="row">
+ 	<div class="col s12 m1 l1"></div>
+
+ 	<div class="col s12 m10 l10">
+
+
+ 		<h4 class="card-panel teal lighten-2 center-align white-text">Salones de Clases</h4>
+ 	 	
+		 <div class="col s12 m12 l4">
+		    <h4 class="header">Teoria</h4>
+		    <div class="card horizontal">
+		      <div class="card-image">
+		        <img height="180px" width="280px" src="http://chicsudc.wikispaces.com/file/view/libro.jpg/130675001/227x175/libro.jpg">
+		      </div>
+		      <div class="card-stacked">
+		        <div class="card-content">
+		          <p>Clases Teoricas</p>
+		        </div>
+		        <div class="card-action">
+		          <a href="maestros.php">Titular</a>
+		        </div>
+		      </div>
+		    </div>
+		  </div>
+
+		   <div class="col s12 m12 l4">
+		    <h4 class="header">Teoria</h4>
+		    <div class="card horizontal">
+		      <div class="card-image">
+		        <img width="100px" height="180px" src="http://previews.123rf.com/images/matheesaengkaew/matheesaengkaew1307/matheesaengkaew130700002/21049378-Mujer-cocinando-Foto-de-archivo.jpg">
+		      </div>
+		      <div class="card-stacked">
+		        <div class="card-content">
+		          <p>Clases Teoricas</p>
+		        </div>
+		        <div class="card-action">
+		          <a href="maestros.php">Titular</a>
+		        </div>
+		      </div>
+		    </div>
+		  </div>
+
+		   <div class="col s12 m12 l4">
+		    <h4 class="header">Teoria</h4>
+		    <div class="card horizontal">
+		      <div class="card-image">
+		        <img width="100px" height="180px" src="http://chicsudc.wikispaces.com/file/view/libro.jpg/130675001/227x175/libro.jpg">
+		      </div>
+		      <div class="card-stacked">
+		        <div class="card-content">
+		          <p>Clases Teoricas</p>
+		        </div>
+		        <div class="card-action">
+		          <a href="maestros.php">Titular</a>
+		        </div>
+		      </div>
+		    </div>
+		  </div>
+
+
+ 
+
+
+ </div>
+
+
+	</div>
+
+ </body>
+ </html>
